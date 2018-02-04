53d8882a9a3c05b82723525cf6aa586fc29f2f31
fouad <1>
2018-02-04 20:39:19 +0300

    asd

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,30 +5,37 @@ public class Main {
     public static void main(String[] args) {
 
 //------------------------------ Class car ----------------
+/*
 
-      /*  car camry = new car();
+  car camry = new car();
         camry.speed = 10;
         int sp = camry.speed;
 
         System.out.println(sp);
-*/
 
-    /*  car max = new car();
+
+
+  car max = new car();
       max.setSpeed(120);
-      //int sp = max.getSpeed();*/
 
+
+
+*/
 
 
 
 
 //------------------------------ Class IF ----------------
 
-    /*    iF ff = new iF();
+/*
+    iF ff = new iF();
         ff.setspeed(10);
 
         int y = ff.getSpeed();
-        System.out.println(y);*/
+        System.out.println(y);
 
+
+*/
 
 
 
@@ -36,25 +43,25 @@ public class Main {
 
 //------------------------------ Class switch ----------------
 
+/*
         // type method int ****
 
-      /*  Switch sw = new Switch();
+  Switch sw = new Switch();
         sw.Speed(44);
-*/
         // type method String ****
-/*
 
         Switch sn = new Switch();
         sn.Myname("d");
 
         String h = sn.name;
         System.out.println(h);
+
+
 */
 
 
-
-
 //------------------------------ Class For Loop ----------------
+
 /*
 
         Loop loo = new Loop();
@@ -65,26 +72,34 @@ public class Main {
         loo.countdown(5);
         loo.countdown(10);
         loo.dec(20);
-*/
 
+
+
+*/
 
 
 
 //------------------------------ Class For While ----------------
+/*
 
-     /*   While wl = new While();
+   While wl = new While();
         wl.do_while();
         wl.dec(7);
+
 */
+
 
 
 
 
 //------------------------------ Class String ----------------
 
-       /* String1 st = new String1();
+/*
+ String1 st = new String1();
         st.text1();
         System.out.println(st);
+
+
 */
 
 
@@ -92,12 +107,16 @@ public class Main {
 
 //------------------------------ Class Array ----------------
 
-       /* Araay1 ar = new Araay1();*/
+/*
+ Araay1 ar = new Araay1();
+
 
         // الاوبجيكت الخاصة بدالة printArray ويتم التعويض عن القيم هنا في البراميتر
 
-       /* int val[] = {11,33,44,77,88,90};
-        ar.MyArray2D();*/
+ int val[] = {11,33,44,77,88,90};
+        ar.MyArray2D();
+
+*/
 
 
 //------------------------------ Class Calculator ----------------
@@ -115,15 +134,18 @@ public class Main {
 
         int resulte4 = cal.div(20,3);
         System.out.println(resulte4);
+
 */
 
 //------------------------------ Class Example Level 1 ----------------
 
+       /*
         Example1 exa = new Example1();
         exa.site();
 
+*/
 
-
+       // anwer shehata
 
     }
 
